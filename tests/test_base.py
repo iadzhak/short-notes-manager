8cfaa94cbbed7f90aa8bad3e64e06c71e2c1ac97
Sergei Iadzhak <yadzhak@yandex.ru>
2026-02-14 21:15:41 +0300

    test_base.py

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,16 +5,15 @@ from src.models.base import Base
 
 
 class TestBase:
+    pk1 = uuid4()
+    pk2 = uuid4()
 
     def test_create(self):
-        pk = uuid4()
-        new = Base(pk)
-        assert new.id == pk
+        new = Base(self.pk1)
+        assert new.id == self.pk1
 
     def test_cant_change_id(self):
-        pk1 = uuid4()
-        pk2 = uuid4()
-        new = Base(pk1)
+        new = Base(self.pk1)
         with pytest.raises(AttributeError):
-            new.id = pk2
-        assert new.id == pk1
+            new.id = self.pk2
+        assert new.id == self.pk1
