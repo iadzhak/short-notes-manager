6178be51516e99627115b05b64469dcc52b9b281
Sergei Iadzhak <yadzhak@yandex.ru>
2026-02-14 21:57:37 +0300

    category + serializable

--- src/models/category.py
+++ src/models/category.py
@@ -1,10 +1,14 @@
 from typing import Self
 from uuid import uuid4
 
+from src.mixins.serializable import Serializable
+
 from .entity import Entity
 
 
-class Category(Entity):
+class Category(Entity, Serializable):
+    serializable_fields = ('id', 'title')
+
     @classmethod
     def create(cls, title: str) -> Self:
         return cls(id=uuid4(), title=title)
